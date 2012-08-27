0857ed291ef0bcb0b661f8ce4af3e5e545ea90d1
fbueller <fbueller@box.(none)>
2012-08-27 22:35:40 +0100

    Updated captchas to use the general unique hash function for the captchaKey in the session.

--- application/models/general_model.php
+++ application/models/general_model.php
@@ -17,21 +17,7 @@ class General_model extends CI_Model {
 	}
 
 
-	// Determine the required authorization for a page based on first URI segment.
-	public function getAuthLevel($URI){
-		// Select required level from the DB.
-		$this->db->select('authLevel')
-			 ->where(array('URI' => $URI));
-		$query = $this->db->get('pageAuthorization');
-		if($query->num_rows() > 0){
-			// If the entry exists, return the value to the table.
-			$row = $query->row_array();
-			return $row['authLevel'];
-		} else {
-			// Failed, return false. Force users to log in.
-			return false;
-		}
-	}
+
 };
 
 

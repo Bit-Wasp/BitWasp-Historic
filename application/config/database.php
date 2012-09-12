51bc620157030df946b20538d09dba52d86965c2
fbueller <fbuellerpriv@gmail.com>
2012-09-12 14:30:37 +0200

    Update application/config/database.php

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'a0sdf7a8sdylf';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
 $db['default']['database'] = 'bitwasp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'bw_';

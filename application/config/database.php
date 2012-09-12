619f65c46ba8c6f0541e899bcf39919debf737e2
thomas <thomas@the-lab.(none)>
2012-09-12 11:09:07 +0000

    test

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'a073ygjahkdg';
 $db['default']['database'] = 'bitwasp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'bw_';

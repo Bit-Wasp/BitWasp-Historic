92e8f3801f7d82626571117c6f8170d975ad4169
Polonium <polonium@the-gibson>
2012-09-15 13:34:28 +0100

    Fix closing tag

--- application/views/users/register.php
+++ application/views/users/register.php
@@ -1,62 +1,62 @@
           <div class="offset3 span6">
             <h2>Register</h2>
             <?php echo form_open('users/register', array('class' => 'form-horizontal')); ?>
-            <fieldset>
-              <?php if(isset($returnMessage)) echo $returnMessage; ?>
-              <div class="control-group">
-                <label class="control-label" for="username">Username</label>
-                <div class="controls">
-                  <input type='text' name='username' value="<?php echo set_value('username'); ?>" size='12' />
-                  <span class="help-inline"><?php echo form_error('username'); ?></span>
-                </div>
-              </div> 
+              <fieldset>
+                <?php if(isset($returnMessage)) echo $returnMessage; ?>
+                <div class="control-group">
+                  <label class="control-label" for="username">Username</label>
+                  <div class="controls">
+                    <input type='text' name='username' value="<?php echo set_value('username'); ?>" size='12' />
+                    <span class="help-inline"><?php echo form_error('username'); ?></span>
+                  </div>
+                </div> 
 
-              <div class="control-group">
-                <label class="control-label" for="password0">Password</label>
-                <div class="controls">
-                  <input type='password' name='password0' value='' size='12' />
-                  <span class="help-inline"><?php echo form_error('password0'); ?></span>
+                <div class="control-group">
+                  <label class="control-label" for="password0">Password</label>
+                  <div class="controls">
+                    <input type='password' name='password0' value='' size='12' />
+                    <span class="help-inline"><?php echo form_error('password0'); ?></span>
+                  </div>
                 </div>
-              </div>
 
-              <div class="control-group">
-                <label class="control-label" for="password1">Password (confirm)</label>
-                <div class="controls">
-                  <input type='password' name='password1' value='' size='12' />
-                  <span class="help-inline"><?php echo form_error('password1'); ?></span>
+                <div class="control-group">
+                  <label class="control-label" for="password1">Password (confirm)</label>
+                  <div class="controls">
+                    <input type='password' name='password1' value='' size='12' />
+                    <span class="help-inline"><?php echo form_error('password1'); ?></span>
+                  </div>
                 </div>
-              </div>
 
-            <div class="control-group">
-                <label class="control-label" for="usertype">Role</label>
-                <div class="controls">
-                  <select name='usertype' value='1'>
-                    <option value='1'>Buyer</option>
-                    <option value='2'>Seller</option>
-                  </select>
-                  <span class="help-inline"><?php echo form_error('usertype'); ?></span>
+                <div class="control-group">
+                  <label class="control-label" for="usertype">Role</label>
+                  <div class="controls">
+                    <select name='usertype' value='1'>
+                      <option value='1'>Buyer</option>
+                      <option value='2'>Seller</option>
+                    </select>
+                    <span class="help-inline"><?php echo form_error('usertype'); ?></span>
+                  </div>
                 </div>
-              </div>
 
-             <!-- Captcha -->
-             <div class="control-group">
-                <label class="control-label" for="captcha">Captcha</label>
-                <div class="controls">
-                  <div class="captcha-img"><?=$captcha['image'];?></div>
+                <!-- Captcha -->
+                <div class="control-group">
+                  <label class="control-label" for="captcha">Captcha</label>
+                  <div class="controls">
+                    <div class="captcha-img"><?=$captcha['image'];?></div>
+                  </div>
                 </div>
-              </div>
-              <div class="control-group">
-                <div class="controls">
-                  <input type="text" name='captcha' size='12'/>
-                  <span class="help-inline"><?php echo form_error('captcha'); ?></span>
+                <div class="control-group">
+                  <div class="controls">
+                    <input type="text" name='captcha' size='12'/>
+                    <span class="help-inline"><?php echo form_error('captcha'); ?></span>
+                  </div>
                 </div>
-              </div>
-              <!-- /Captcha -->
+                <!-- /Captcha -->
 
-              <div class="form-actions">
-                <button type='submit' class="btn btn-primary">Register</button>
-                <?=anchor('users/login', 'Cancel', 'title="Cancel" class="btn"');?>
-              </div>
-            </fieldset>
-          </form>
-
+                <div class="form-actions">
+                  <button type='submit' class="btn btn-primary">Register</button>
+                  <?=anchor('users/login', 'Cancel', 'title="Cancel" class="btn"');?>
+                </div>
+              </fieldset>
+            </form>
+          </div>
